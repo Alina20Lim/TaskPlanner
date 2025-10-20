36ce46ee7fa2aa3ddb343d6f57b647312eff7608
Alina20Lim <168667890+Alina20Lim@users.noreply.github.com>
2025-10-20 19:01:17 +0300

    initial commit - created console application

---
+++ JonDou9000.TaskPlanner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JonDou9000.TaskPlanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
